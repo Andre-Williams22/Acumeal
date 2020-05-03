d656464ad496c7b729b734bdeaf9a9fac13d2ba3
Andre Williams <awjrs22@gmail.com>
2020-05-03 15:05:10 -0700

    adds meals to database. Reads in high and low cal meals into database but will not send data to user account because they need to login first

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -7,6 +7,14 @@ from sklearn.preprocessing import LabelEncoder
 import numpy as np 
 import pandas as pd
 import pickle
+import os
+import tablib
+import csv
+# dataset = tablib.Dataset()
+# with open(os.path.join(os.path.dirname(__file__),'High-Cal.csv')) as df:
+#     dataset.csv = df.read()
+#df = pd.read_csv('High-Cal.csv', encoding='cp1252')
+
 posts = [
     {
         'author': 'Andre Williams',
@@ -78,28 +86,47 @@ def quiz():
 
         if prediction == 1:
             print('Meal 1')
-            df = pd.read_csv('../Datasets/Acumeal_ New Recipe Database - High-Cal.csv')
-            df = df.iloc[0:6]
-            week = df['High Calorie Plan']
-            breakfast = df['Breakfast1']
-            lunch = df['Lunch']
-            dinner = df['Dinner']
-            snack = df['Snack']
-            total = df['Total']
-            
+            # df = pd.read_csv(request.file.get('High-Cal.csv'))
+            with open(os.path.join(os.path.dirname(__file__),'High-Cal.csv')) as readfile:
+                df = pd.read_csv(readfile)
+            # with open(os.path.join(os.path.dirname(__file__),'High-Cal.csv')) as df:
+                # dataset.csv = df.read()
+                # df = dataset.csv
+                df = df.iloc[0:6]
+                week = df['High Calorie Plan'].iloc[0]
+                breakfast = df['Breakfast1'].iloc[0]
+                lunch = df['Lunch'].iloc[0]
+                dinner = df['Dinner'].iloc[0]
+                snack = df['Snack'].iloc[0]
+                total = df['Total'].iloc[0]
+                measurement = df['Measurement'].iloc[0]
+                
+                week_1 = Meal(week=week, breakfast=breakfast, lunch=lunch, dinner=dinner, snack=snack, total=total, measurement=measurement, user_id=current_user.id)
+                db.session.add(week_1)
+                db.session.commit()
+                flash(f"You're meal plan is ready {form.first.data} " + f"{form.last.data}! Please Login to View Meal Plan", 'success')
+                return redirect(url_for('login'))     
+
+        elif prediction == 0:
+            print('Meal 2')
+            data = pd.read_csv('Low-Cal.csv')
+            data = data.iloc[0:6]
+            data['Low Calorie Plan']
+            lunch = data['Lunch']
+            dinner = data['Dinner']
+            snack = data['Snack']
+            total = data['Total']
+            week = data['High Calorie Plan']
+
             week_1 = Meal(week=week, breakfast=breakfast, lunch=lunch, dinner=dinner, snack=snack, total=total)
+            # save prediction in database 
             db.session.add(week_1)
             db.session.commit()
             flash(f"You're meal plan is ready {form.first.data} " + f"{form.last.data}! Please Login to View Meal Plan", 'success')
             return redirect(url_for('login'))     
-
-
-        else:
-            print('Meal 2')
-
-        # save prediction in database 
-
+        
         # grab 
+
 
        # mealplan = Mealplan()
 
